fcf437fbb448a825cbcddb34669d10c79759f9d6
MonsterBasket <blazza1@hotmail.com>
2022-07-18 14:30:28 +1000

    this is going to take a long time to really be *comfortable* with

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solution in this file!
+function returnFirstTwoDrivers(array){
+    return array.slice(0,2);
+}
+function returnLastTwoDrivers(array){
+    return array.slice(array.length-2);
+}
+
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(fare){
+    return (multiplier) => fare * multiplier;
+}
+function fareDoubler(fare){
+    return createFareMultiplier(fare)(2);
+}
+// function fareTripler(fare){
+//     return createFareMultiplier(fare)(3);
+// }
+const fareTripler = (fare) => createFareMultiplier(fare)(3); 
+//Is this still a const?  Or is it now just a function?  Is there a difference?
+
+function selectDifferentDrivers(array, func){
+    return func(array);
+}
